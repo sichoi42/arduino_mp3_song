10efece6fb5c20c46291d93c3a0a6904c44aeb16
sichoi <42.4.sichoi@gmail.com>
2021-11-24 13:30:56 +0900

    ⚡️ add recorder

--- awesome_piano_main/awesome_piano_main.ino
+++ awesome_piano_main/awesome_piano_main.ino
@@ -3,11 +3,12 @@
 /*select play mode*/
 #define PIANO 1
 #define MP3 2
-#define RECODER 3
+#define RECORDER 3
+/****************/
 /*define for mp3*/
 #define R -1
 #define BIT_1 600
-
+/***************/
 /*fixed Tone library*/
 #include <Tone.h>
 #include "SPARKLE.h"
@@ -73,11 +74,90 @@ void play_mp3()
       speaker[1].play(h[1], h_dur[1]);
     cnt++;
   
-    //delay(m_dur + 10);
+    delay(m_dur);
     //delay(h_dur + 10);
-    delay(min(m_dur, h_dur[0], h_dur[1]));
+    //delay(min(m_dur, h_dur[0], h_dur[1]));
     if (cnt == len)
         flag = PAINO;
+}
+
+int16_t record_melody[6][50][2];
+
+void recording()
+{
+    static int    checker[6] = {0, };
+    int           m[6] = {0, };
+    int           m_dur[6] = {0, };
+    int           m_start[6] = {0, };
+    
+    for (int i=0;i<6;i++)
+    {
+        if (i == 0 && digitalRead(8) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_C4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (i == 1 && digitalRead(9) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_D4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (i == 2 && digitalRead(10) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_E4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (i == 3 && digitalRead(11) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_F4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (i == 4 && digitalRead(12) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_G4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (i == 5 && digitalRead(13) == 0)
+        {
+            if (checker[i] == 0)
+            {
+                m_start[i] = millis();
+                checker[i] = 1;
+            }
+            m[i] = NOTE_A4;
+            m_dur[i] = millis() - m_start[i];
+        }
+        if (digitalRead(i + 8) == 1)
+        {
+            checker[i] = 0;
+            m_start[i] = 0;
+        }
+    }
 }
 
 void loop()
@@ -85,6 +165,10 @@ void loop()
     play_piano();
     if (digitalRead(0) == 0)
         flag = MP3;
+    if (digitalRead(1) == 0)
+        flag = RECORDER;
     if (flag == MP3)
         play_mp3();
+    if (flag == RECORDER)
+        recording();
 }
